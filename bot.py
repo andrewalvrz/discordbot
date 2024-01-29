9af6902d681da50cca22fe5ac38def88a6e274e8
dexzzy <148374373+dexzzy@users.noreply.github.com>
2024-01-29 11:49:39 -0600

    Create bot.py
    TOKEN is left blank as you will insert your own token given to you by discord.

---
+++ bot.py
@@ -0,0 +1,42 @@
+import discord
+import responses
+
+intents = discord.Intents.all()  # Use intents.all() to enable all default intent flags
+
+async def send_message(message, user_message, is_private):
+    try:
+        response = responses.handle_response(user_message)
+        await message.author.send(response) if is_private else await message.channel.send(response)
+    except Exception as e:
+        print(e)
+
+def run_discord_bot():
+    TOKEN = ''
+    client = discord.Client(intents=intents)  # Pass the intents parameter
+
+    @client.event
+    async def on_ready():
+        print(f'{client.user} is now running')
+
+    @client.event
+    async def on_message(message):
+        if message.author == client.user:
+            return 
+
+        username = str(message.author)
+        user_message = str(message.content)
+        channel = str(message.channel)
+
+        print(f"{username} said: '{user_message}' ({channel})")
+
+        if user_message.startswith('?'):
+            user_message = user_message[1:]
+            await send_message(message, user_message, is_private=True)
+        else:
+            await send_message(message, user_message, is_private=False)
+
+    # Call the run function with the provided token
+    client.run(TOKEN)
+
+# Call the run_discord_bot function to start the bot
+run_discord_bot()
